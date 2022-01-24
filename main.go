e39f54a2017c36cafbc8e85093c95a7462b6076f
aalkan <abdullah_alkan17@hotmail.com>
2022-01-24 22:39:51 +0300

    excel styling done

--- main.go
+++ main.go
@@ -50,8 +50,18 @@ func main() {
 
 		f := excelize.NewFile()
 		// Create a new sheet.
+		style, _ := f.NewStyle(`{"alignment":{"vertical":"top"}}`)
+		styleLast, _ := f.NewStyle(`{"fill":{"type":"pattern","color":["#9ACD32"],"pattern":4}}`)
+		bold, _ := f.NewStyle(`{"font":{"bold":true}}`)
+
 		index := f.NewSheet("Sheet1")
 		// Set value of a cell.
+
+		f.SetColWidth("Sheet1", "A", "H", 20)
+		f.SetColStyle("Sheet1", "A:H", style)
+		f.SetColStyle("Sheet1", "E", styleLast)
+		f.SetColStyle("Sheet1", "A", bold)
+
 		f.SetCellValue("Sheet1", "A1", "Domain")
 		f.SetCellValue("Sheet1", "B1", "Kayıtlı Kuruluş")
 		f.SetCellValue("Sheet1", "C1", "Kayıt Tarihi")
@@ -65,42 +75,40 @@ func main() {
 		f.SetActiveSheet(index)
 		// Save spreadsheet by the given path.
 
-		randomName := pkg.GetWr() + "/" + "results.xlsx"
-
 		for i, domain := range domains {
 			result, _ := whois.GetWhois(domain)
 			lines := strings.Split(result, "\n")
-			f.SetCellValue("Sheet1", fmt.Sprintf("A%x", i+2), domain)
+			f.SetCellValue("Sheet1", fmt.Sprintf("A%d", i+2), domain)
 			ns1Flasg := false
 			for _, line := range lines {
-
 				if strings.Contains(line, "Registrar WHOIS Server") {
 					value := strings.Split(line, ":")[1]
-					f.SetCellValue("Sheet1", fmt.Sprintf("B%x", i+2), value)
-
+					f.SetCellValue("Sheet1", fmt.Sprintf("B%d", i+2), value)
 				} else if strings.Contains(line, "Creation Date") {
 					value := strings.Split(line, ":")[1]
-					f.SetCellValue("Sheet1", fmt.Sprintf("C%x", i+2), value)
+					f.SetCellValue("Sheet1", fmt.Sprintf("C%d", i+2), value)
 				} else if strings.Contains(line, "Updated Date") {
 					value := strings.Split(line, ":")[1]
-					f.SetCellValue("Sheet1", fmt.Sprintf("D%x", i+2), value)
+					f.SetCellValue("Sheet1", fmt.Sprintf("D%d", i+2), value)
 				} else if strings.Contains(line, "Registry Expiry Date") {
 					value := strings.Split(line, ":")[1]
-					f.SetCellValue("Sheet1", fmt.Sprintf("E%x", i+2), value)
-				} else if strings.Contains(line, "Name Server") {
+					f.SetCellValue("Sheet1", fmt.Sprintf("E%d", i+2), value)
+				} else if strings.Contains(line, "Name Server") && !ns1Flasg {
 					value := strings.Split(line, ":")[1]
-					f.SetCellValue("Sheet1", fmt.Sprintf("F%x", i+2), value)
+					f.SetCellValue("Sheet1", fmt.Sprintf("F%d", i+2), value)
 					ns1Flasg = true
 				} else if strings.Contains(line, "Name Server") && ns1Flasg {
 					value := strings.Split(line, ":")[1]
-					f.SetCellValue("Sheet1", fmt.Sprintf("G%x", i+2), value)
+					f.SetCellValue("Sheet1", fmt.Sprintf("G%d", i+2), value)
 					ns1Flasg = true
 				} else if strings.Contains(line, "Registrant Country") {
 					value := strings.Split(line, ":")[1]
-					f.SetCellValue("Sheet1", fmt.Sprintf("H%x", i+2), value)
+					f.SetCellValue("Sheet1", fmt.Sprintf("H%d", i+2), value)
 				}
 			}
 		}
+
+		randomName := pkg.GetWr() + "/" + "results.xlsx"
 		if err := f.SaveAs(randomName); err != nil {
 			fmt.Println(err.Error() + " Dosya kayıt edilemedi")
 		}
